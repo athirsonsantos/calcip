cf505b3be61ebf4403b6c6ebaa7cb5c195c1e1ed
Athirson Santos <titoalesantoszzz@gmail.com>
2018-07-05 16:54:24 -0300

    Add files via upload

---
+++ Calculadora IP/calculadoraip.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $("#botao1").click(function () {
+            
+
+            });
+        });
+    </script>
+</head>
+<body>
+
+<form method="get">
+    <input type="number" id="ip1" name="ip">
+    .
+    <input type="number" id="ip2" name="ip">
+    .
+    <input type="number" id="ip3" name="ip">
+    .
+    <input type="number" id="ip4" name="ip">
+    /
+    <input type="number" id="masc" name="masc">
+    <input type="submit" id="calc" value="Calcular">
+</form>
+
+
+
+
+</body>
+
+</html>
---
+++ Calculadora IP/ip2.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aluno
+ * Date: 05/07/18
+ * Time: 16:26
+ */
+$ip = $_GET('ip');
+$mascara = 25;
+$octetos = explode(".",$ip);
